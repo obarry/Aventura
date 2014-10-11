2d31749d653c3171a9a23997eff580326805f119
Bricolage Olivier <Bricolage Olivier>
2014-10-11 20:49:47 +0000

    Cerate Model class + Javadoc

---
+++ src/com/aventura/model/Model.java
@@ -0,0 +1,9 @@
+package com.aventura.model;
+
+public class Model {
+	private String name; // name of the model
+	// TBD all attributes of a model
+	
+	private Element root; // root of the tree of Elements of this model
+
+}
