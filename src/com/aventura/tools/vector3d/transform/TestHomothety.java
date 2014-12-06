ffdb411d354c785af083bc5b26efa34bd52bff9f
Bricolage Olivier <Bricolage Olivier>
2014-12-06 23:07:43 +0000

    Implementation of tests for TestHomothety

--- src/com/aventura/tools/vector3d/transform/TestHomothety.java
+++ src/com/aventura/tools/vector3d/transform/TestHomothety.java
@@ -1,19 +1,64 @@
 package com.aventura.tools.vector3d.transform;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import com.aventura.tools.vector3d.matrix.*;
 
 public class TestHomothety {
 
 	@Test
 	public void testHomothety() {
-		fail("Not yet implemented");
+		System.out.println("***** Test Homotethy : testHomotethy *****");
+		
+		Homothety h = new Homothety(12);
+		System.out.println("Homothety h: "+ h);
+		
+		double[][] a = {
+				{12.0, 0.0, 0.0}, 
+	            {0.0, 12.0, 0.0},
+				{0.0, 0.0, 12.0}};
+		
+		try {
+			Matrix3 m = new Matrix3(a);
+			System.out.println("Matrix m: "+ m);
+			
+			assertTrue(h.equals(m));
+			System.out.println("h = m -> success");
+			
+		} catch (MatrixArrayWrongSizeException e) {
+			e.printStackTrace();
+			fail("WrongSizeException");
+		} 
+			
 	}
 
 	@Test
 	public void testSetScale() {
-		fail("Not yet implemented");
+		System.out.println("***** Test Homotethy : testSetScale *****");
+		
+		Homothety h = new Homothety();
+		System.out.println("Homothety h: "+ h);
+		
+		double[][] a = {
+				{7.0, 0.0, 0.0}, 
+	            {0.0, 7.0, 0.0},
+				{0.0, 0.0, 7.0}};
+		
+		try {
+			Matrix3 m = new Matrix3(a);
+			System.out.println("Matrix m: "+ m);
+			
+			h.setScale(7);
+			System.out.println("Homothety h after setScale(7): "+ h);
+		
+			assertTrue(h.equals(m));
+			System.out.println("h = m -> success");
+			
+		} catch (MatrixArrayWrongSizeException e) {
+			e.printStackTrace();
+			fail("WrongSizeException");
+		} 
+			
 	}
 
 }
