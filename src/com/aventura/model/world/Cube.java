897b10c99e9a0e87fb39d85ee37b7e899cd072f8
obarry <olivier38b@gmail.com>
2016-05-15 15:28:14 +0200

    Fix constructor issue

--- src/com/aventura/model/world/Cube.java
+++ src/com/aventura/model/world/Cube.java
@@ -1,5 +1,11 @@
 package com.aventura.model.world;
 
+import com.aventura.math.vector.Vector3;
+
 public class Cube extends Box {
+	
+	public Cube(double length, Vector3 position) {
+		super(length, length, length, position);
+	}
 
 }
