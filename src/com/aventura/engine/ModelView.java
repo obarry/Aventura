9debb700c61db30072b3a0118c4616dd2ece920b
obarry <olivier38b@gmail.com>
2016-05-22 18:13:15 +0200

    Add missing ModelView class

---
+++ src/com/aventura/engine/ModelView.java
@@ -0,0 +1,55 @@
+package com.aventura.engine;
+
+import com.aventura.model.world.Element;
+
+/**
+ * This class represents and manages the transformations from model to view.
+ * 
+ * Interesting explanations on this transformation can be found here:
+ * http://www.opengl-tutorial.org/fr/beginners-tutorials/tutorial-3-matrices/
+ * 
+ *     +---------------------+
+ *     |  Model Coordinates  |
+ *     +---------------------+
+ *                |
+ *                |   [Model Matrix] 4x4 Transformation Matrix
+ *                v
+ *     +---------------------+
+ *     |  World Coordinates  |
+ *     +---------------------+
+ *                |
+ *                |   [View Matrix] 4x4 Transformation Matrix
+ *                v
+ *     +----------------------+
+ *     |  Camera Coordinates  |
+ *     +----------------------+
+ *                |
+ *                |   [Projection Matrix] 4x4 Transformation Matrix
+ *                v
+ *   +--------------------------+
+ *   | Homogeneous Coordinates  |
+ *   +--------------------------+
+ * 
+ *  It provides all needed services to the RenderEngine to compute these transformations for each Vertex of the model.
+ *  
+ *  Each Transformation is a set of 1) Scaling then 2) Rotation and last 3) Translation.
+ *  This can be modeled by the following matrix and vectors computation:
+ *  
+ *  	TransformedVector = [TranslationMatrix * RotationMatrix * ScaleMatrix] * OriginalVector
+ *  	TransformedVector = [4x4 Transformation Matrix] * OriginalVector
+ *  
+ *  The complete transformation, from model to homogeneous coordinates, is done through the following formula:
+ *  
+ *  	TransformedVector = [Projection Matrix] * [View Matrix] * [Model Matrix] * OriginalVector
+ *  
+ *  Note that the Model Matrix is provided by the model itself.
+ *  In Aventura model, it is at Element level and it may require to be multiplied recursively if the model contains sub-elements.
+ *  See Element class description.
+ *  
+ * @author Bricolage Olivier
+ * @since May 2016
+ * 
+ */
+public class ModelView {
+
+}
