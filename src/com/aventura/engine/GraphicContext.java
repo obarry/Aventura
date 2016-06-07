f0a3f01ffb07d816ea0eca28ff249e34c481e986
obarry <obarry@169.254.114.171>
2016-06-07 23:17:23 +0200

    Start developing TestAventura with default construction of a RenderEngine. Move forward in the implementation of the various contexts.

--- src/com/aventura/engine/GraphicContext.java
+++ src/com/aventura/engine/GraphicContext.java
@@ -1,8 +1,34 @@
 package com.aventura.engine;
 
+/**
+ * 
+ * @author Bricolage Olivier
+ * @since May 2016
+ *
+ */
 public class GraphicContext {
 	
+	public static final int PERSPECTIVE_TYPE_FRUSTUM = 1;
+	public static final int PERSPECTIVE_TYPE_ORTHOGRAPHIC = 2;
+
+
+	// Window & frustum
 	int width;
 	int height;
+	
+	int perspective_type = 0;
+	
+	public GraphicContext() {
+		
+	}
+	
+	public GraphicContext(int width, int height, int type) {
+		this.width = width;
+		this.height = height;
+		this.perspective_type = type;
+	}
+	
+	public static GraphicContext GRAPHIC_DEFAULT = new GraphicContext(800,450,PERSPECTIVE_TYPE_FRUSTUM);
+	
 
 }
--- src/com/aventura/model/light/Lighting.java
+++ src/com/aventura/model/light/Lighting.java
@@ -1,5 +1,22 @@
 package com.aventura.model.light;
 
+import java.util.ArrayList;
+
 public class Lighting {
+	
+	protected ArrayList<Light> lights; // There can be multiple sources of light
+	
+	public Lighting() {
+		
+	}
+	
+	public Lighting(Light light) {
+		lights.add(light);
+	}
+	
+	public void addLight(Light light) {
+		lights.add(light);
+	}
+
 
 }
