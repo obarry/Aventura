4a64ebe0bfb447606283257a27701930bb7f0aab
obarry <obarry@169.254.114.171>
2016-06-26 22:50:48 +0200

    Add new concrete class SwingView

---
+++ src/com/aventura/view/SwingView.java
@@ -0,0 +1,17 @@
+package com.aventura.view;
+
+public class SwingView extends View {
+
+	@Override
+	public void drawPixel(int x, int y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawLine(int x1, int y1, int x2, int y2) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
