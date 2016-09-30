d987df33ebadd2430c70214a0d8e15675a4ea653
obarry <obarry@OBARRY4.emea.hpqcorp.net>
2016-09-30 18:11:17 +0200

    Start implementation of Sphere

--- src/com/aventura/model/world/Sphere.java
+++ src/com/aventura/model/world/Sphere.java
@@ -1,5 +1,9 @@
 package com.aventura.model.world;
 
+import com.aventura.math.transform.Translation;
+import com.aventura.math.vector.Vector3;
+import com.aventura.math.vector.Vector4;
+
 /**
  * ------------------------------------------------------------------------------ 
  * MIT License
@@ -25,9 +29,66 @@ package com.aventura.model.world;
  * SOFTWARE.
  * ------------------------------------------------------------------------------ 
  * 
+ *  Vertices: +
+ *  Segments: / \ | -
+ *  Exemple;
+ *  Number of segments: 12
+ *  -> 12 segments around the sphere horizontally (360 degrees) -> 12 vertices (only, not 13!)
+ *  -> 6 segments from north to south (180 degrees) -> north pole, 5 vertices, south pole (n+1)
+ *  -> table of 12 x 5 vertices + 2 poles
+ * 
+ *               +					North Pole
+ *    / / / / / /|\ \ \ \ \            
+ *   +-+-+-+-+-+-+-+-+-+-+-+-
+ *   | | | | | | | | | | | |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-
+ *   | | | | | | | | | | | |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-
+ *   | | | | | | | | | | | |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-
+ *   | | | | | | | | | | | | 
+ *   +-+-+-+-+-+-+-+-+-+-+-+-
+ *    \ \ \ \ \ \|/ / / / / 
+ *               +					South Pole
+ * 
+ *   
+ * 
  * @author Bricolage Olivier
  * @since May 2016
  */
 public class Sphere extends Element {
+	
+	protected Vertex[][] vertices;
+	
+	/**
+	 * @param ray
+	 * @param half_seg is half the number of segments for 360 degrees
+	 * @param position
+	 */
+	public Sphere(double ray, int half_seg, Vector3 position) {
+		super();
+		subelements = null;
+		vertices = new Vertex[half_seg*2][half_seg+1]; // (n) x (n/2+1) vertices (n being the number of segments)
+		Vertex northPole, southPole;
+		double alpha = Math.PI/half_seg;
+		
+		// Create Vertices
+		northPole = new Vertex(new Vector4(0, 0, ray,  1));
+		southPole = new Vertex(new Vector4(0, 0, -ray,  1));
+		
+		for (int i=0; i<half_seg*2; i++) {
+			for (int j=0; j<half_seg+1; j++) {
+				vertices[i][j] = new Vertex(new Vector4(ray*Math.cos(alpha*i), ray*Math.sin(alpha*i), 0,  1));
+			}
+		}
+		
+		// Create Triangles - 2 triangles per "square" face
+		
+		// Complete creation of this element with a translation
+		this.transform = new Translation(position);
+
+	}
+
+
 
 }
