9c9718cd337d18fb1e79f7b7e7e599432560e6d3
obarry <olivier38b@gmail.com>
2017-07-16 23:24:48 +0200

    Implementation of FanMesh in progress. TestRasterizer has been designed to test FanMesh.

--- src/com/aventura/model/world/triangle/FanMesh.java
+++ src/com/aventura/model/world/triangle/FanMesh.java
@@ -1,5 +1,7 @@
 package com.aventura.model.world.triangle;
 
+import com.aventura.math.vector.Vector4;
+import com.aventura.model.texture.Texture;
 import com.aventura.model.world.Vertex;
 import com.aventura.model.world.shape.Element;
 
@@ -58,7 +60,84 @@ public class FanMesh extends Mesh {
 	public FanMesh(Element e, int n) {
 		super(e);
 		this.nbv = n;
+		summit = new Vertex();
 		vertices = elm.createVertexMesh(n);
+	}
+	
+	public FanMesh(Element e, int n, Texture t) {
+		super(e);
+		this.nbv = n;
+		this.tex = t;
+		summit = new Vertex();
+		vertices = elm.createVertexMesh(n);
+	}
+
+	public void createTriangles(int type) {
+
+		Triangle t;
+
+		switch (type) {
+
+		case MESH_ORIENTED_TRIANGLES:
+			// (n) vertices -> (n-1) triangles
+
+			for (int i=0; i<nbv-1; i++) {
+
+				// Creation of triangles
+				//
+				//   summit  +
+				//         / |
+				//       +---+
+				//       i  i+1
+
+			
+				t = new Triangle(vertices[i], vertices[i+1], summit);
+
+				// Texture application on the FanMesh, assuming regular stitches
+				if (tex!=null) {
+
+					Vector4 tv1, tv2, tv3;
+
+					// Define position for Texture vectors in homogeneous coordinates [0,1]
+					double ti = (double)i/(double)(nbv-1);
+					double tip1 = (double)(i+1)/(double)(nbv-1);
+					double tsx = 0.0001;
+					double tsz = 0.0002;
+
+
+					// Create texture vectors with an horizontal 'stretching' texture effect (on the tip of the fan) 
+					tv1 = new Vector4(ti,0,0,1);
+					tv2 = new Vector4(0,tsx,0,tsz);
+					tv3 = new Vector4(tip1,0,0,1);
+
+					// Set texture vectors to newly created triangles
+					//t1.setTexture(new Vector4(0,0,0,1), new Vector4(1,0.0001,0,0.0002), new Vector4(0,1,0,1));
+
+					t.setTextureOrientation(Triangle.TEXTURE_HORIZONTAL);
+					t.setTexture(tex, tv1, tv2, tv3);
+
+				}
+
+				elm.addTriangle(t);
+			}
+			break;
+
+		default:
+			// Invalid type
+			return;
+		}	
+	}
+
+	public Vertex[] getVertices() {
+		return vertices;
+	}
+	
+	public Vertex getVertex(int i) {
+		return vertices[i];
+	}
+	
+	public Vertex getSummit() {
+		return summit;
 	}
 
 }
