54a223c6aab819d3bd019db0a023ac491f5bf2bc
obarry <olivier38b@gmail.com>
2017-10-24 00:28:02 +0200

    Improve TestTorusTexture

--- src/com/aventura/test/TestTorusTexture.java
+++ src/com/aventura/test/TestTorusTexture.java
@@ -71,7 +71,7 @@ public class TestTorusTexture {
 		//Texture tex = new Texture("resources/test/texture_bricks_204x204.jpg");
 		//Texture tex = new Texture("resources/test/texture_blueground_204x204.jpg");
 		//Texture tex = new Texture("resources/test/texture_woodfloor_160x160.jpg");
-		Texture tex = new Texture("resources/test/texture_damier_600x591.gif");
+		//Texture tex = new Texture("resources/test/texture_damier_600x591.gif");
 		//Texture tex = new Texture("resources/test/texture_grass_900x600.jpg");
 		//Texture tex = new Texture("resources/test/texture_ground_stone_600x600.jpg");
 		//Texture tex = new Texture("resources/test/texture_snow_590x590.jpg");
@@ -83,6 +83,8 @@ public class TestTorusTexture {
 		//Texture tex = new Texture("resources/test/texture_sticker_cremedemarrons_351x201.jpg", Texture.TEXTURE_DIRECTION_VERTICAL, Texture.TEXTURE_ORIENTATION_NORMAL, Texture.TEXTURE_ORIENTATION_OPPOSITE);
 		//Texture tex = new Texture("resources/test/texture_rust_960x539.jpg");
 		//Texture tex = new Texture("resources/test/texture_carpet_600x600.jpg");
+		//Texture tex = new Texture("resources/test/texture_blue_checkboard_1300x1300.jpg");
+		Texture tex = new Texture("resources/test/texture_geometry_1024x1024.jpg");
 
 		// Camera
 		Vector4 eye = new Vector4(15,5,10,1);
