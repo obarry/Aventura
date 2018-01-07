ea03410d60da7af7bc546b8b3ef554532f0d81a2
obarry <olivier38b@gmail.com>
2018-01-07 23:17:36 +0100

    Change Copyright to 2018

--- src/com/aventura/test/TestView.java
+++ src/com/aventura/test/TestView.java
@@ -19,7 +19,7 @@ import com.aventura.view.View;
 * ------------------------------------------------------------------------------ 
 * MIT License
 * 
-* Copyright (c) 2017 Olivier BARRY
+* Copyright (c) 2018 Olivier BARRY
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
