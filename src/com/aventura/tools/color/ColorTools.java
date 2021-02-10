972942ad1740d49dbeefe9214bd466612e07c3d1
Olivier Barry <olivier.barry@hpe.com>
2021-02-11 00:35:56 +0100

    Update and add Copyright headers to 2021

--- src/com/aventura/tools/color/ColorTools.java
+++ src/com/aventura/tools/color/ColorTools.java
@@ -8,7 +8,7 @@ import com.aventura.math.vector.Tools;
  * ------------------------------------------------------------------------------ 
  * MIT License
  * 
- * Copyright (c) 2020 Olivier BARRY
+ * Copyright (c) 2021 Olivier BARRY
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
